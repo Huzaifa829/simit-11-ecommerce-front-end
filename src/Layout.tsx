aa2f474f7e7720d01649609a218f924fcb7b1d15
Huzaifa Ahmed <huzaifaahmed829@gmail.com>
2025-01-10 07:05:12 +0500

    done

---
+++ src/Layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+      {/* <Navbar/> */}
+      <Outlet/>
+    </>
+  )
+}
+
+export default Layout
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import { createBrowserRouter } from 'react-router-dom'
+const route = createBrowserRouter([
+  {
+    path:'/',
+    
+  }
+])
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
